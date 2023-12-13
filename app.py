e1fe2e08d5de3af26acbdd4ab8082ddb63605360
tregu0458 <54461561+tregu148@users.noreply.github.com>
2023-12-13 10:35:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import gradio as gr
-import os
+import subprocess
 
-def greet(name):
-    return "Hello " + name + "!" + name
+def run_command(cmd):
+    try:
+        result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        return e.stderr
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=run_command, inputs="text", outputs="text")
 
 if __name__ == "__main__":
     demo.launch(show_api=False, server_name="0.0.0.0")
